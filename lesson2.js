f8ac6cd3a2b8de7ad08e10d5fcd02dd76556d1b3
suralokin1 <kolya_romashkin@bk.ru>
2023-07-26 11:46:11 +0300

    Создал код JS

---
+++ lesson2.js
@@ -0,0 +1,27 @@
+// Задание 1
+
+// Переменная для сохранения температуры в градусах Цельсия
+// var celsius = 25;
+
+// // Перевевожу температуру в градусы Фаренгейта
+// var fahrenheit = (9 / 5) * celsius + 32;
+
+// // Вывожу температуру в градусах Цельсия и Фаренгейта
+// console.log("Temperature in Celsius: " + celsius + "°C");
+// console.log("Temperature in Fahrenheit: " + fahrenheit + "°F");
+
+// Задание 2
+
+//Создаю переменную name
+var name = "Nikolay";
+//Решил немного поиграться с переменными, показать свои знания
+// var name = "Nikolay"
+// var surname = "Romashkin"
+// var age = "24"
+
+// Создаю другую переменную и присваиваю ей значение переменной "name"
+ var admin = name;
+
+// // Вывожу значение переменной "admin" в консоль
+// console.log(name,surname,age);
+    console.log(admin);
